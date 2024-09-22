2e224519cfdacf9c7e8fe5c41d8ef184c3cb07db
Kyrylo Prylipko <24914627+reend@users.noreply.github.com>
2024-09-23 01:02:53 +0300

    update

--- boot.asm
+++ boot.asm
@@ -3,17 +3,14 @@ org 0x7C00
 use16
 
 start:
-    ; Установим стек
     mov ax, 0x07C0
     add ax, 288
     mov ss, ax
     mov sp, 4096
 
-    ; Очистка экрана
     mov ax, 0x03
     int 0x10
 
-    ; Вывод строки
     mov si, message
 print_char:
     lodsb
@@ -26,7 +23,7 @@ print_char:
 hang:
     jmp hang
 
-message db 'Hello, world!', 0
+message db 'Booting...', 0
 
 times 510-($-$$) db 0
 dw 0xAA55
